1d895ccd5fa7a032eda4b0e91bc117e2e90ab258
thales <tabdon.sousa@contavoltz.com>
2024-06-18 16:41:07 -0300

    feat: create search bar functions

--- js/script.js
+++ js/script.js
@@ -1,21 +1,65 @@
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
+document.addEventListener('DOMContentLoaded', function () {
+  const swiper = new Swiper('.swiper', {
     direction: 'horizontal',
     loop: true,
-  
-    // If we need pagination
     pagination: {
       el: '.swiper-pagination',
     },
-  
-    // Navigation arrows
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
-  
-    // And if we need scrollbar
     scrollbar: {
       el: '.swiper-scrollbar',
     },
   });
+
+  const gameInput = document.getElementById('gameInput');
+  const searchIcon = document.getElementById('searchIcon');
+
+ 
+  gameInput.addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+      const searchGame = gameInput.value.trim().toUpperCase();
+      
+      switch (searchGame) {
+        case 'MARIO':
+          swiper.slideTo(0); 
+          break;
+        case 'POKEMON':
+          swiper.slideTo(1); 
+          break;
+        case 'ZELDA':
+          swiper.slideTo(2); 
+          break;
+        default:
+          alert('Jogo não encontrado.');
+          break;
+      }
+
+      gameInput.value = '';
+    }
+  });
+
+  
+    searchIcon.addEventListener('click', function () {
+    const searchGame = gameInput.value.trim().toUpperCase();
+
+    switch (searchGame) {
+      case 'MARIO':
+        swiper.slideTo(0); 
+        break;
+      case 'POKEMON':
+        swiper.slideTo(1); 
+        break;
+      case 'ZELDA':
+        swiper.slideTo(2); 
+        break;
+      default:
+        alert('Jogo não encontrado.');
+        break;
+    }
+
+    gameInput.value = '';
+  });
+});
